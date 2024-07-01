5033de329aabef410ca6cdd71cd796e38ca35d86
bgirlalma <124092247+bgirlalma@users.noreply.github.com>
2024-07-01 14:16:59 +0000

    Deploying to gh-pages from @ bgirlalma/it-portfolio@88c34d6999b51e3e772ee633aef7dd64ebeac9e8 🚀

--- assets/ErrorPage-0aef0072.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-bdfebbb2.js";const o=()=>r.jsx("div",{children:r.jsx("h1",{children:"Error"})});export{o as default};
---
+++ assets/ErrorPage-12b9457b.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-ba26c4b9.js";const o=()=>r.jsx("div",{children:r.jsx("h1",{children:"Error"})});export{o as default};
